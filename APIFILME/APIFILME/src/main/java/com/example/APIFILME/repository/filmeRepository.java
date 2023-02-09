bd1bf20c52f3eca9d720649bb493d000beed1281
Adriana Brunner <a.drk63@gmail.com>
2023-02-09 11:08:13 -0300

    UPDATE

--- APIFILME/APIFILME/src/main/java/com/example/APIFILME/repository/filmeRepository.java
+++ APIFILME/APIFILME/src/main/java/com/example/APIFILME/repository/filmeRepository.java
@@ -8,11 +8,10 @@ import java.util.List;
 
 public interface filmeRepository extends JpaRepository<filmeModel, Integer> {
 
-    @Query(value = "SELECT * FROM ADRI_FILMES_BCK "+
-            "WHERE DATA_LANCAMENTO = ?1 ", nativeQuery = true)
+    @Query(value = "SELECT * FROM ADRI_FILMES_BCK WHERE DATA_LANCAMENTO = ?1 ", nativeQuery = true)
     public List<filmeModel> findbyDATA_LANCAMENTO(int DATA_LANCAMENTO);
 
-    @Query(value = "SELECT * FROM ADRI_FILMES_BCK WHERE PAIS_ORIGEM = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM ADRI_FILMES_BCK WHERE UPPER(TRIM(PAIS_ORIGEM) = UPPER(PAIS_ORIGEM) ?1", nativeQuery = true)
     public List<filmeModel> findbyPAIS_ORIGEM(String PAIS_ORIGEM);
 
     @Query(value = "SELECT * FROM ADRI_FILMES_BCK ORDER BY QUANTIDADE_VENDAS DESC", nativeQuery = true)
